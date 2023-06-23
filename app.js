dd20cbeb50bc265647d47d2100da39cf1bf8cbd8
GobuuriNaresh <nareshgobburi2001>
2023-06-23 18:21:54 +0530

    creatingRequest

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server=http.createServer((req,res)=>{
-    console.log('naresh');
-    process.exit();
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
 
-})
+});
 server.listen(4000);
