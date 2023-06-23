3e742ce4b37daa565804747907c2fa4c4b88d2be
GobuuriNaresh <nareshgobburi2001>
2023-06-23 18:37:32 +0530

    responseToTheRequest

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 const server=http.createServer((req,res)=>{
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-type' ,'text/html');
     //process.exit();
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my node.js server!</h1></body>');
+    res.write('</html>');
+    res.end();
+
+
+
 
 });
 server.listen(4000);
